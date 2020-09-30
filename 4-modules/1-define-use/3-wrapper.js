9631a3666e18b48b36e9dee396d2ac20d9f23eb2
Alex Costan <acostan.con@orangetheory.com>
2020-09-30 14:02:14 -0400

    defining and using node modules

--- 4-modules/1-define-use/3-wrapper.js
+++ 4-modules/1-define-use/3-wrapper.js
@@ -1,1 +1,15 @@
-console.log(arguments);
+// function (exports, module, require, __filename, __dirname) {
+
+let g = 1 // not a global variable, scoped to the wrapping node function
+
+console.log(arguments)
+
+exports.a = 42 // arguement to the hidden node wrapping function
+module.exports = 37
+
+// exports = () => {} => not ok
+module.exports = () => {} // ok!
+
+// return module.exports => every node file returns this
+
+// }
