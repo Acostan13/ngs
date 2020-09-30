3bf2dd4f2e4e7916f345544489156f2dbf071676
Alex Costan <acostan.con@orangetheory.com>
2020-09-30 16:19:04 -0400

    using the req, res objects

--- 4-modules/5-errors/2-try.js
+++ 4-modules/5-errors/2-try.js
@@ -5,8 +5,7 @@ const files = ['.bash_profile', 'kjkjhh', '.npmrc'];
 
 files.forEach(file => {
   try {
-    const filePath = path.resolve(process.env.HOME, file);
-    const data = fs.readFileSync(filePath);
+    const data = fs.readFileSync(file, 'utf-8');
     console.log('File data is', data);
   } catch (err) {
     console.log('File not found');
--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
-});
+const requestListener = (req, res) => {
+  // req => readable stream
+  console.dir(req, {depth: 0}) // prints the incoming response
+  
+  // res => writable stream
+  console.dir(res, {depth: 0}) // prints the server response
+  
+  res.write('Hello World\n');
+  res.end()
+}
+
+const server = http.createServer(requestListener);
+server.on('request', requestListener);
 
 server.listen(4242, () => {
   console.log('Server is running...');
